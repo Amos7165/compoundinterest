f907d806e9032258fc1a18dc2b314e1b941039c7
root <root@localhost>
2024-09-25 13:30:23 +0300

    auto publish 1727260222783

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+ //declaration and initialization 
+ float p,r,t,ci;
+ printf("enter principle value:");
+ scanf("%f",p);
+ printf("enter rate values:");
+ scanf("%f",t);
+ ci=p*((pow((1+r/100),t)-1);
+ print(compound interest:%.2f"ci");
+ return 0;
+ }
+ 
+ 
